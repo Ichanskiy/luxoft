43b528bea41765c46b81d51541c4b138f511889e
Ichanskiy <ichanskiy.olexandr@gmail.com>
2019-02-18 13:51:23 +0200

    configure project

---
+++ src/main/java/com/ichanskyi/luxoft/LuxoftApplication.java
@@ -0,0 +1,13 @@
+package com.ichanskyi.luxoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuxoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ichanskyi/luxoft/LuxoftApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ichanskyi.luxoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LuxoftApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
